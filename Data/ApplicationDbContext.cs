fc51fac3a2143d9a501a5c1c935d06fb37f9383c
lecon20122 <lecon20122@gmail.com>
2023-06-21 14:23:33 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Areas.Identity.Data;
+using Pharmacy.Models;
+
+namespace Pharmacy.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<PharmacyUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Pharmacy.Models.Inventory> Inventory { get; set; } = default!;
+    }
+}
